d7190b5fc0d24e751b14c4347d5a49bd5f53b966
shadod <scout0501@gmail.com>
2025-11-11 10:02:30 +0300

    feat(main): added latest action

---
+++ src/components/LatestActions.tsx
@@ -0,0 +1,38 @@
+import {useCollectionListInfinite} from "@context/CollectionListInfiniteContext";
+import type {ActionRecord} from "@shared/types/action";
+import {UserAction} from "./profile/UserAction";
+import {Flex, For, Heading} from "@chakra-ui/react";
+import {useInView} from "react-intersection-observer";
+
+export const LatestActions = () => {
+    const {
+        data: actions,
+        isFetching,
+        hasNextPage,
+        fetchNextPage,
+    } = useCollectionListInfinite<ActionRecord>();
+
+    const {ref: bottomRef} = useInView({
+        onChange: async (inView) => {
+            if (inView && hasNextPage && !isFetching) {
+                await fetchNextPage();
+            }
+        },
+        trackVisibility: true,
+        delay: 100,
+    });
+
+    return (
+        <Flex direction="column" gap={5} align="center">
+            <Heading>Последние ходы</Heading>
+            <For each={actions.pages}>
+                {(list) => (
+                    list.items.map((action) => (
+                        <UserAction key={action.id} action={action}/>
+                    ))
+                )}
+            </For>
+            <div ref={bottomRef}></div>
+        </Flex>
+    )
+}
--- src/components/pages/Main.tsx
+++ src/components/pages/Main.tsx
@@ -1,15 +1,27 @@
 import {Board} from "../board/Board";
 import {useAppContext} from "@context/AppContextProvider/AppContextProvider";
 import {UserMenu} from "../board/UserMenu";
-import {Box} from "@chakra-ui/react";
+import {Box, Spacer} from "@chakra-ui/react";
+import {LatestActions} from "../LatestActions";
+import {CollectionListInfiniteProvider} from "@context/CollectionListInfiniteContext";
+import {Sort} from "@shared/hook/useSort";
 
 export const Main = () => {
-    const {isAuth} = useAppContext();
+    const {pb, isAuth} = useAppContext();
 
     return (
         <>
             <Box position="relative">
                 <Board/>
+                <Spacer h={10}/>
+                <CollectionListInfiniteProvider
+                    collection={pb.collection('actions')}
+                    expand='game,cell'
+                    pageSize={10}
+                    initialSort={new Map([['created', Sort.DESC]])}
+                >
+                    <LatestActions/>
+                </CollectionListInfiniteProvider>
             </Box>
             {isAuth ? <UserMenu/> : null}
         </>
