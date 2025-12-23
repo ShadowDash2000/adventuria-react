f8cd02e40214ac743dac523113c75895822fa8b3
shadod <scout0501@gmail.com>
2025-12-23 07:08:49 +0300

    feat(radio): added media keys support

--- src/components/radio/RadioBackgroundController.tsx
+++ src/components/radio/RadioBackgroundController.tsx
@@ -1,13 +1,13 @@
-import { useAppAuthContext } from '@context/AppContext';
 import { AudioKey, useAudioPlayer } from '@shared/hook/useAudio';
 import { useEffect } from 'react';
 import { useRadioStore } from './useRadioStore';
 import { useRadioPreset } from './useRadioPreset';
+import { useRadio } from '@components/radio/useRadio';
 
 export const RadioBackgroundController = () => {
-    const { pb } = useAppAuthContext();
-    const { onEnded, play } = useAudioPlayer(AudioKey.radio);
-    const { nextIndex, currentAudioIndex, setAudioCount } = useRadioStore();
+    const { setAudioCount } = useRadioStore();
+    const { onEnded } = useAudioPlayer(AudioKey.radio);
+    const { audioName, play, pause, isPlaying, prevAudio, nextAudio } = useRadio();
 
     const { data } = useRadioPreset();
 
@@ -17,16 +17,35 @@ export const RadioBackgroundController = () => {
 
     useEffect(() => {
         const unsubscribe = onEnded(async () => {
-            nextIndex();
-
-            const audioList = data?.expand?.audio ?? [];
-            const nextAudio = audioList[(currentAudioIndex + 1) % (audioList.length || 1)];
-            if (nextAudio) {
-                await play(pb.files.getURL(nextAudio, nextAudio.audio));
-            }
+            nextAudio();
         });
         return () => unsubscribe();
-    }, [onEnded, nextIndex, data, currentAudioIndex, play, pb]);
+    }, [onEnded, nextAudio]);
+
+    useEffect(() => {
+        if (!('mediaSession' in navigator)) return;
+
+        if (audioName) {
+            navigator.mediaSession.metadata = new MediaMetadata({
+                title: audioName,
+                artist: 'Приключпопия Team',
+            });
+        }
+
+        navigator.mediaSession.playbackState = isPlaying ? 'playing' : 'paused';
+
+        navigator.mediaSession.setActionHandler('play', () => play());
+        navigator.mediaSession.setActionHandler('pause', () => pause());
+        navigator.mediaSession.setActionHandler('previoustrack', () => prevAudio());
+        navigator.mediaSession.setActionHandler('nexttrack', () => nextAudio());
+
+        return () => {
+            navigator.mediaSession.setActionHandler('play', null);
+            navigator.mediaSession.setActionHandler('pause', null);
+            navigator.mediaSession.setActionHandler('previoustrack', null);
+            navigator.mediaSession.setActionHandler('nexttrack', null);
+        };
+    }, [isPlaying, audioName, play, pause, prevAudio, nextAudio]);
 
     return null;
 };
