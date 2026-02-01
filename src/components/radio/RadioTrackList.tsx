39c0544d242bbfec3b1e6e79aa5817b607415ff8
shadod <scout0501@gmail.com>
2026-02-01 17:19:33 +0300

    feat(RadioTrackList): added scroll to active track

--- src/components/radio/RadioTrackList.tsx
+++ src/components/radio/RadioTrackList.tsx
@@ -1,6 +1,6 @@
 import { Listbox, useListCollection } from '@chakra-ui/react';
 import type { AudioRecord } from '@shared/types/audio';
-import { useEffect, useState } from 'react';
+import { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import type { RecordIdString } from '@shared/types/pocketbase';
 
 interface RadioTrackListProps {
@@ -16,6 +16,7 @@ export const RadioTrackList = ({ audioId, audio, onTrackChange }: RadioTrackList
         itemToString: item => item.name,
         itemToValue: item => item.id,
     });
+    const activeItemRef = useRef<HTMLDivElement | null>(null);
 
     useEffect(() => {
         setCollection(audio || []);
@@ -24,6 +25,12 @@ export const RadioTrackList = ({ audioId, audio, onTrackChange }: RadioTrackList
     useEffect(() => {
         if (audioId) setValue([audioId]);
     }, [audioId, setValue]);
+
+    useLayoutEffect(() => {
+        if (activeItemRef.current) {
+            activeItemRef.current.scrollIntoView({ block: 'nearest', behavior: 'smooth' });
+        }
+    }, [audioId, collection.items.length]);
 
     return (
         <Listbox.Root
@@ -36,7 +43,11 @@ export const RadioTrackList = ({ audioId, audio, onTrackChange }: RadioTrackList
         >
             <Listbox.Content maxH="{sizes.32}">
                 {collection.items.map(audio => (
-                    <Listbox.Item item={audio} key={audio.id}>
+                    <Listbox.Item
+                        item={audio}
+                        key={audio.id}
+                        ref={audio.id === audioId ? activeItemRef : null}
+                    >
                         <Listbox.ItemText>{audio.name}</Listbox.ItemText>
                         <Listbox.ItemIndicator />
                     </Listbox.Item>
