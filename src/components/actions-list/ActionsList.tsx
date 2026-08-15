90a4cdc986c4ff4a4d7358732e978f0d0cf9ce5d
shadod <scout0501@gmail.com>
2026-08-16 00:47:12 +0300

    feat(actions-list): display text when no actions found

--- src/components/actions-list/ActionsList.tsx
+++ src/components/actions-list/ActionsList.tsx
@@ -1,11 +1,11 @@
-import { Box, Flex, For, Spinner, Text } from '@chakra-ui/react';
+import { Box, Flex, For, Spinner, Text, VStack } from '@chakra-ui/react';
 import type { ClientResponseError } from 'pocketbase';
 import { useInView } from 'react-intersection-observer';
 import { PlayerAction } from './PlayerAction';
 import { useActionsListContext } from './ActionsListContext';
 
 export const ActionsList = () => {
-    const { pages, error, isPending, isError, isFetchingNextPage, fetchNextPage } =
+    const { pages, totalItems, error, isPending, isError, isFetchingNextPage, fetchNextPage } =
         useActionsListContext();
 
     const { ref: bottomRef } = useInView({
@@ -30,9 +30,17 @@ export const ActionsList = () => {
 
     return (
         <>
-            <For each={pages}>
-                {list => list.items.map(action => <PlayerAction key={action.id} action={action} />)}
-            </For>
+            {(totalItems > 0 && (
+                <For each={pages}>
+                    {list =>
+                        list.items.map(action => <PlayerAction key={action.id} action={action} />)
+                    }
+                </For>
+            )) || (
+                <VStack h={48} justify="center">
+                    <Text>Ничего не найдено</Text>
+                </VStack>
+            )}
             <div ref={bottomRef} style={{ height: '10px' }} />
             <Box minH="32px" display="flex" alignItems="center" justifyContent="center">
                 {isFetchingNextPage ? <Spinner /> : null}
